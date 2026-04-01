aebe69dfa2e43150dde3f361d47ba9c2f6dd2eac
fumiya <altema1217bbfumi@live.jp>
2026-04-01 20:46:30 +0900

    fix: 環境変数HONO_API_URLを使用してクライアントのURLを設定

--- nextjs/src/lib/client.ts
+++ nextjs/src/lib/client.ts
@@ -1,4 +1,4 @@
 import { hc } from 'hono/client'
 import type { AppType } from 'hono-api/src/index'
 
-export const client = hc<AppType>('http://localhost:3001')
+export const client = hc<AppType>(process.env.HONO_API_URL ?? 'http://localhost:3001')
