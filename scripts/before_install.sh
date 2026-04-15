1cd4f22b893d5f67908e49971adeb5dacc1d48e5
fumiya <altema1217bbfumi@live.jp>
2026-04-16 01:28:12 +0900

    fix

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
 
+cp /home/ec2-user/nextjs-workspace/.env /temp/.env.backup 2>dev/null || true
 rm -rf /home/ec2-user/nextjs-workspace
+mkdir -p /home/ec2-user/nextjs-workspace
+cp /tmp/.env.backup /home/ec2-user/nextjs-workspace/.env 2>/dev/null || true
